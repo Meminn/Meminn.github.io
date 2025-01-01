0734f831a741c9541690c09244a0b067ec686fba
memin <meminbayrak55@gmail.com>
2025-01-01 15:46:39 +0300

    Refactor role return logic in Sidebar component to return segments directly

--- my-portfolio/src/components/Sidebar.vue
+++ my-portfolio/src/components/Sidebar.vue
@@ -43,7 +43,7 @@ export default {
       } else if (roleParam === 'a') {
         return 'ANDROID DEVELOPER';
       } else {
-        return segments[1].toUpperCase();
+        return segments;
       }
     },
   },
